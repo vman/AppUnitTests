574b1bd6fbfc5f7b3d1aff9599c772f247295f12
vman <vardhaman.rd@gmail.com>
2015-05-05 20:00:48 +0100

    o365unified mvc

---
+++ Personal/Office365.Unified.API/O365.Unified.API/Helpers/SettingsHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace O365.Unified.API.Helpers
+{
+    public class SettingsHelper
+    {
+        public static string ClientId
+        {
+            get { return ConfigurationManager.AppSettings["ida:ClientID"]; }
+        }
+
+        public static string ClientSecret
+        {
+            get { return ConfigurationManager.AppSettings["ida:Password"]; }
+        }
+
+        public static string AzureAdTenantId
+        {
+            get { return ConfigurationManager.AppSettings["ida:TenantId"]; }
+        }
+
+        public static string AzureADAuthority
+        {
+            get { return "https://login.microsoftonline.com/common"; }
+        }
+
+        public static string O365UnifiedResource
+        {
+            get { return "https://graph.microsoft.com/"; }
+        }
+    }
+}
